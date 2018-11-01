4a16682e33b726d5dc61ce00f2f1a38bf2f87916
Jenny Kam <jkam17@gmail.com>
2018-11-01 15:57:49 -0400

    set up get and post actions for signing up

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,43 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'users/new'
+    if session[:user_id] != nil
+      redirect '/users/:slug' #already signed in!
+    else
+      erb :'users/new'
+    end
+  end
+
+  post '/signup' do
+    if params[:user][:username].empty? || params[:user][:password].empty?
+      redirect '/signup' #incomplete sign up form!
+    else
+      user = User.create(params[:user])
+      session[:user_id] = user.id
+      redirect '/users/:slug'
+    end
   end
 
   get '/login' do
-    erb :'users/login'
+    if session[:user_id] != nil
+      redirect '/users/:slug'
+    else
+      erb :'users/login'
+    end
   end
+
+  post '/login' do
+    user = User.find_by(username: params[:username])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect '/users/:slug'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/users/:slug' do
+
+  end
+
 end
