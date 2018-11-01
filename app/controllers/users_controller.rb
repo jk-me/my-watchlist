eae82c5f6176cb9288e7319819f53dfcf0a717ee
Jenny Kam <jkam17@gmail.com>
2018-11-01 16:32:41 -0400

    remove text in controller that appeared due to error

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,8 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-<<<<<<< HEAD
     if session[:user_id] != nil
       redirect '/users/:slug' #already signed in!
-=======
-    if session[:user_id] != nil #already signed in!
-      redirect '/users/:slug'
->>>>>>> 558ad12ea4ae4f151ada20f20cd431edaf3298bf
     else
       erb :'users/new'
     end
@@ -15,11 +10,7 @@ class UsersController < ApplicationController
 
   post '/signup' do
     if params[:user][:username].empty? || params[:user][:password].empty?
-<<<<<<< HEAD
       redirect '/signup' #incomplete sign up form!
-=======
-      redirect '/signup'
->>>>>>> 558ad12ea4ae4f151ada20f20cd431edaf3298bf
     else
       user = User.create(params[:user])
       session[:user_id] = user.id
@@ -45,14 +36,6 @@ class UsersController < ApplicationController
     end
   end
 
-<<<<<<< HEAD
-=======
-  post '/login' do
-
-    redirect '/users/:slug'
-  end
-
->>>>>>> 558ad12ea4ae4f151ada20f20cd431edaf3298bf
   get '/users/:slug' do
 
   end
