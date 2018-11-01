558ad12ea4ae4f151ada20f20cd431edaf3298bf
Jenny Kam <jkam17@gmail.com>
2018-11-01 15:57:49 -0400

    set up get and post actions for logging in

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,34 @@
 class UsersController < ApplicationController
 
   get '/signup' do
-    erb :'users/new'
+    if session[:user_id] != nil #already signed in!
+      redirect '/users/:slug'
+    else
+      erb :'users/new'
+    end
+  end
+
+  post '/signup' do
+    if params[:user][:username].empty? || params[:user][:password].empty?
+      redirect '/signup'
+    else
+      user = User.create(params[:user])
+      session[:user_id] = user.id
+      redirect '/users/:slug'
+    end
   end
 
   get '/login' do
     erb :'users/login'
   end
+
+  post '/login' do
+
+    redirect '/users/:slug'
+  end
+
+  get '/users/:slug' do
+
+  end
+
 end
