06030d600f6ae1485b40d3ad8b6b9d9d82c46138
Jenny Kam <jkam17@gmail.com>
2018-11-02 23:49:22 -0400

    add bootstrap css layout, add current_user helper function

--- app/controllers/app_controller.rb
+++ app/controllers/app_controller.rb
@@ -25,6 +25,10 @@ class ApplicationController < Sinatra::Base
         redirect "/users/#{session[:user_id]}"
       end
     end
+
+    def current_user
+      User.find(session[:user_id])
+    end
   end
 
 end
