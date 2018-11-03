9f5c779870a2173faff94d8b8d5518682f6422e0
Jenny Kam <jkam17@gmail.com>
2018-11-02 21:50:35 -0400

    set up edit and delete routes for shows

--- app/controllers/shows_controller.rb
+++ app/controllers/shows_controller.rb
@@ -7,11 +7,31 @@ class ShowsController < ApplicationController
 
   post '/shows' do
     if params[:show][:name].empty?
-      redirect '/shows/new' #incomplete submission! must have movie name!
+      redirect '/shows/new' #incomplete submission! must have name!
     else
       s = Show.create(params[:show])
       s.update(user_id: session[:user_id])
       redirect "/users/#{session[:user_id]}"
     end
+  end
+
+  get '/shows/:id/edit' do
+    redirect_if_not_logged_in
+    #correct user check
+    @show = Show.find(params[:id])
+    erb :'shows/edit'
+  end
+
+  patch '/shows/:id' do
+    s = Show.find(params[:id])
+    s.update(params[:show])
+    redirect "/users/#{session[:user_id]}"
+  end
+
+  delete '/shows/:id' do
+    s = Show.find(params[:id])
+    s.delete
+    redirect "/users/#{session[:user_id]}"
+
   end
 end
